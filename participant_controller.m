2d4a0968501218e2a76305eb0179f0fd1cb8bed2
jgvictores <jcgvicto@gmail.com>
2021-07-04 20:27:59 +0200

    add view motors

--- participant_controller.m
+++ participant_controller.m
@@ -1,6 +1,10 @@
 % Initialize the robot
 timestep = wb_robot_get_basic_time_step();
 
+% Initialize devices
+motor_left = wb_robot_get_device('wheel_left_joint');
+motor_right = wb_robot_get_device('wheel_right_joint');
+
 % Main loop:
 % - perform simulation steps until Webots is stopping the controller
 while wb_robot_step(timestep) ~= -1
