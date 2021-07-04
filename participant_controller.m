3e7a03c2f71d8cf8fa57294141a0b1954e61e6f5
jgvictores <jcgvicto@gmail.com>
2021-07-04 20:09:18 +0200

    add robot step loop

--- participant_controller.m
+++ participant_controller.m
@@ -1,1 +1,8 @@
-wb_console_print(sprintf('Hello World from MATLAB!\n'), WB_STDOUT);
+% Initialize the robot
+timestep = wb_robot_get_basic_time_step();
+
+% Main loop:
+% - perform simulation steps until Webots is stopping the controller
+while wb_robot_step(timestep) ~= -1
+  wb_console_print(sprintf('Hello World from MATLAB!\n'), WB_STDOUT);
+end
